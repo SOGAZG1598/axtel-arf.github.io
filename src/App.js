53692cbc543c88c885e628c0743199dea9dfed6b
SOGAZG1598 <sogazg@hotmail.com>
2026-03-29 23:01:42 -0500

    vscode 3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 // ============================================================
 // PORTAL DE REQUERIMIENTOS DE FIREWALL
